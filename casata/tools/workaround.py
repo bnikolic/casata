1af95b3c24e5417bf87c0fbacb1777b6c691ad67
Bojan Nikolic <bojan@bnikolic.co.uk>
2010-04-14 15:37:55 +0100

    Add the workaround for pointing table frame problems

---
+++ casata/tools/workaround.py
@@ -0,0 +1,49 @@
+# 2010 Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
+#
+# This file is part of casata and is licensed under GPL version 2
+"""
+Tools for working around problems in CASA which areconsidered fixable
+in the future
+"""
+
+import tempfile
+import shutil
+
+def fixPointingFill(msin):
+    """
+    Fix problems with pointing table in present in some datasets
+    filled from the ASDM.
+
+    The current implementation is to export to uvfits and reimport and
+    then reuse the pointing table
+    """
+    fitsname=tempfile.mktemp(suffix="fits")
+    exportuvfits(vis=msin,
+                 fitsfile=fitsname,
+                 datacolumn="data",
+                 field="",
+                 spw="0",
+                 antenna="",
+                 timerange="",
+                 nchan=-1,
+                 start=0,
+                 width=1,
+                 writesyscal=False,
+                 multisource=True,
+                 combinespw=True,
+                 writestation=True)
+    msnewname=tempfile.mktemp(suffix="ms")
+    importuvfits(fitsfile=fitsname,
+                 vis=msnewname,
+                 antnamescheme="old")
+    oldpoint=os.path.join(msin,
+                          "POINTING")
+    newpoint=os.path.join(msnewname,
+                          "POINTING")
+    shutil.rmtree(oldpoint)
+    shutil.copytree(newpoint,
+                    oldpoint)
+    shutil.rmtree(msnewname)
+    
+    
+    
