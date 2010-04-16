d66867f0dd000df2e6716baa1fed7884b22efa84
Bojan Nikolic <bojan@bnikolic.co.uk>
2010-04-16 19:46:22 +0100

    Factor out the creation of the calibration table names to a separate function, first quick sketch of gain calibration

--- casata/tools/calibration.py
+++ casata/tools/calibration.py
@@ -10,6 +10,19 @@ import os
 import casata
 from  casata import deco
 
+def calTableName(msin,
+                 caltype,
+                 overwrite=True,
+                 **kwargs):
+    """
+    Create a name for caltables
+    """
+    pref,junk=os.path.splitext(os.path.basename(msin))
+    res=pref+caltype
+    for k in kwargs.keys():
+        res=res+"."+k+str(kwargs[k])
+    return res
+
 @deco.casaGlobD
 def mkDelaySpW(msin,
                delayl,
@@ -22,7 +35,7 @@ def mkDelaySpW(msin,
     :param antenna: Antenna to apply to
     """
     pref,junk=os.path.splitext(os.path.basename(msin))
-    calname=pref+".K"
+    calname=calTableName(msin, "K", antenna=antenna)
     gencal(vis=msin,
            caltable=calname,
            caltype="sbd",
@@ -42,8 +55,7 @@ def mkBandpassChn(msin,
     cb.open(msin)
     cb.selectvis(spw=str(spw),
                  field=calfield)
-    pref,junk=os.path.splitext(os.path.basename(msin))
-    calname=pref+".B"
+    calname=calTableName(msin, "B", spw=spw)
     cb.setsolve(table=calname,    
                 type="B",
                 t="inf",
@@ -56,6 +68,28 @@ def mkBandpassChn(msin,
                     spwmap=[spw])   
     cb.solve()
     cb.close()
+    return calname
+
+def mkGainT(msin,
+            calfield,
+            spw,
+            precal=None):
+    """
+    Compute the time-variable gain. Incomplete
+    """
+    pref,junk=os.path.splitext(os.path.basename(msin))
+    calname=pref+".G"
+    gaincal(vis=msn,
+            caltable=calname,
+            field=calfield,
+            spw=spw,
+            selectdata=True,
+            solint="60s",
+            gaintable=precal,
+            spwmap=[[i],[i]],
+            combine="",refant="0",minblperant=2,minsnr=-1,solnorm=False,
+            gaintype="G",calmode="ap",
+            )
     return calname
     
     
