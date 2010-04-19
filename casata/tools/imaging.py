f72dd4cf4156f66c47434d63600ad689c19f338d
Bojan Nikolic <bojan@bnikolic.co.uk>
2010-04-19 17:30:41 +0100

    First rough go at imaging

---
+++ casata/tools/imaging.py
@@ -0,0 +1,66 @@
+# 2010 Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
+#
+# This file is part of casata and is licensed under GPL version 2
+"""
+Tools for imaging
+"""
+
+import os
+
+import casata
+from  casata import deco
+
+def imageName(msin,
+              field,
+              algo,
+              **kwargs):
+    """
+    Aut-generated names for images
+    """
+    pref,junk=os.path.splitext(os.path.basename(msin))
+    pref+=field
+    return pref
+
+@deco.casaGlobD
+def simpleClean(msin,
+                field,
+                **kwargs):
+    """
+    A simple clean of the input data
+    """
+    clean(vis=msin,
+          imagename=imageName(msin, 
+                              field=field),
+          field=field,
+          **kwargs)    
+
+
+def dirty(msin,
+          field,
+          spw,
+          npix=512,
+          cell=1.0
+          ):
+    """
+    Make the dirty image
+
+    :param cell: Cell size in arcseconds 
+    """
+    im=casac.homefinder.find_home_by_name('imagerHome').create()
+    im.open(msin)
+    im.selectvis(field=field,
+                 spw=spw)
+    im.defineimage(nx=npix, 
+                   ny=npix,
+                   cellx="%garcsec"% cell,
+                   celly="%garcsec"% cell,
+                   phasecenter=0,
+                   spw=[1])
+
+    fnameout=imageName(msin, 
+                       field=field,
+                       algo="dirty")
+    im.makeimage(type='corrected',
+                 image=fnameout)
+
+    
