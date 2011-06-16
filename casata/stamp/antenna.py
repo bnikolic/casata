f7fa3cc1cb25469e62a1ac1cc84674832bc3300b
Bojan Nikolic <bojan@bnikolic.co.uk>
2011-06-16 10:55:28 +0100

    Add note to fix the antenna plotting

--- casata/stamp/antenna.py
+++ casata/stamp/antenna.py
@@ -6,8 +6,12 @@ Quick plots of antenna - related  quantities
 """
 import  os
 
+import pylab
+
 import casata
 from casata.tools import data,  vtasks
+
+
 
 
 def pos(msin):
@@ -16,5 +20,7 @@ def pos(msin):
     """
     pref,junk=os.path.splitext(os.path.basename(msin))
     fnameout=pref+"-antenna-positions.png"
+    # Should do myself in pylab really, since this doesn't work in a
+    # headless session
     vtasks.plotants(msin, fnameout)
     return fnameout
