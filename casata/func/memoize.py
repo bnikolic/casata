837d8c1097ed12de5527984e1e6f2546d758fbf7
Bojan Nikolic <bojan@bnikolic.co.uk>
2011-11-15 12:20:00 +0000

    Python can't hash dict types... Turn the keywords into a list

--- casata/func/memoize.py
+++ casata/func/memoize.py
@@ -20,6 +20,9 @@ def openCache(ms):
 
 def saveCache(ms, d):
     cPickle.dump(d, open(chName(ms), "w"))    
+
+def ordKw(kwl):
+    return [(k, kwl[k]) for k in kwl.keys()]
     
 def MSMemz(f):
     """
@@ -27,7 +30,7 @@ def MSMemz(f):
     """
     def newf(ms, *args, **kwargs):
         c=openCache(ms)
-        k=(f.__name__, args, kwargs)
+        k=(f.__name__, args, ordKw(kwargs))
         if c.has_key(k):
             return c[k]
         else:
