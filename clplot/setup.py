40cbd1c04dadca2c4dcc1e314e5b5d21f8f3e6de
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2016-10-03 10:04:08 +0100

    Close version 0.1

--- clplot/setup.py
+++ clplot/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(name='clplot',
       version='0.1',
-      description='CASA Calculation of closure quantities and plotting',
+      description='CASA calculation of closure quantities and plotting',
 #      url='http://github.com/storborg/funniest',
       author='Bojan Nikolic',
       author_email='b.nikolic@mrao.cam.ac.uk',
-      license='GPL (Like CASA)',
+      license='GPL (same as CASA)',
       packages=['clplot'],
       scripts = ['heraadd.py'],
       zip_safe=False)
